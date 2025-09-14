93bc145a8c9c30c12ee48ce16b92b8a643cb9d07
kartik saxena <kartik03012@gmail.com>
2025-09-14 15:41:03 +0530

    added redis url functionality

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -13,7 +13,12 @@ class RedisClient:
             # Read host and port from environment variables
             redis_host = os.environ.get("REDIS_HOST", "localhost")
             redis_port = int(os.environ.get("REDIS_PORT", 6379))
-            cls._instance = redis.Redis(
-                host=redis_host, port=redis_port, db=0, decode_responses=True
-            )
+            redis_url = os.environ.get("REDIS_URL")
+
+            if not redis_url:
+                cls._instance = redis.Redis(
+                    host=redis_host, port=redis_port, db=0, decode_responses=True
+                )
+            else:
+                cls._instance = redis.from_url(redis_url, decode_responses=True)
         return cls._instance
